c6ed4bd88dc8ef1abdfdda5936e89e8675a943b5
Harold Tomlinson <haroldt@creacom.ca>
2025-04-07 19:34:10 -0400

    moved views

---
+++ ccSchwabManager/Views/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  ccSchwabManager
+//
+//  Created by Harold Tomlinson on 2025-03-26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View
+{
+    @State var positionStatementData: [PositionStatementData] = []
+    var body: some View
+    {
+        TabView
+        {
+            KeychainView( )
+                .tabItem {
+                    Label("Keychain", systemImage: "lock.circle")
+                }
+            SalesCalcView( positionStatementData: $positionStatementData )
+                .tabItem {
+                    Label("Sales Calc", systemImage: "dollarsign.circle")
+                }
+            PositionStatementView( positionStatementData: $positionStatementData )
+                .tabItem {
+                    Label("Position Statement", systemImage: "doc.text")
+                }
+        }
+        .onAppear {
+            let url = Bundle.main.url(forResource: "PositionStatement", withExtension: "csv")
+            positionStatementData = parseCSV(url: url)
+        }
+    }
+
+} // ContentView
+
+
+
+#Preview {
+    ContentView()
+}
