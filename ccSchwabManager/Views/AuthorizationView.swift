84e2cb4a26b37b23eec90891571fd1f3441c4bbc
Harold Tomlinson <haroldt@creacom.ca>
2025-04-19 13:40:52 -0400

    Moving Authorization part of test screen to AuthorizationView.  Code suggested by copilot failed to work without some changes. Next:  Enable dead-code removal in repo.

---
+++ ccSchwabManager/Views/AuthorizationView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthorizationView.swift
+//  ccSchwabManager
+//
+//  Created by Harold Tomlinson on 2025-04-19.
+//
+
+import SwiftUI
+
+struct AuthorizationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthorizationView()
+}
