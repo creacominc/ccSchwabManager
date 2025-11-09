8e7c0efaa12ffb2ef5c5409e5d640659a8d473d2
Harold Tomlinson <haroldt@creacom.ca>
2025-11-08 19:58:37 -0500

    cursor updating for vision

--- ccSchwabManager/Utilities/FontStyles.swift
+++ ccSchwabManager/Utilities/FontStyles.swift
@@ -1,46 +1,49 @@
 import SwiftUI
 
 /// Centralized font styles that support Dynamic Type
-/// Users can adjust font sizes via iOS Settings > Accessibility > Display & Text Size
+/// Users can adjust font sizes via iOS/visionOS Settings > Accessibility > Display & Text Size
 struct FontStyles {
     /// Table cell content font - scales with user's preferred size
-    static let tableCell = Font.system(.body)
+    /// Note: Must be computed property to respond to Dynamic Type changes
+    static var tableCell: Font {
+        Font.system(.body)
+    }
     
     /// Table cell content font with medium weight
-    static let tableCellMedium = Font.system(.body, design: .default, weight: .medium)
+    /// Note: Must be computed property to respond to Dynamic Type changes
+    static var tableCellMedium: Font {
+        Font.system(.body, design: .default, weight: .medium)
+    }
     
     /// Small detail text in tables and lists
-    static let detail = Font.system(.caption)
+    /// Note: Must be computed property to respond to Dynamic Type changes
+    static var detail: Font {
+        Font.system(.caption)
+    }
     
     /// Smaller detail text for secondary information
-    static let detailSmall = Font.system(.caption2)
+    /// Note: Must be computed property to respond to Dynamic Type changes
+    static var detailSmall: Font {
+        Font.system(.caption2)
+    }
     
     /// Header text for tables and sections
-    static let tableHeader = Font.system(.headline)
+    /// Note: Must be computed property to respond to Dynamic Type changes
+    static var tableHeader: Font {
+        Font.system(.headline)
+    }
     
     /// For cases where you need a fixed size (use sparingly)
     /// These sizes will scale with Dynamic Type when accessed through `scaled()` modifier
     enum FixedSize: CGFloat {
-        case small = 14
-        case medium = 16
-        case large = 18
+        case small = 12
+        case medium = 14
+        case large = 16
         
         /// Returns a font that scales with Dynamic Type
         func scaled(weight: Font.Weight = .regular) -> Font {
             return Font.system(size: self.rawValue, weight: weight)
         }
-    }
-}
-
-/// Environment value to track Dynamic Type size changes
-struct DynamicTypeSize: EnvironmentKey {
-    static let defaultValue: ContentSizeCategory = .medium
-}
-
-extension EnvironmentValues {
-    var dynamicTypeSize: ContentSizeCategory {
-        get { self[DynamicTypeSize.self] }
-        set { self[DynamicTypeSize.self] = newValue }
     }
 }
 
